cda12a69b513b625cbca2cc55b5b7bac34b53ba3
Anish Maharjan <anish.maharjan@infotmt.com>
2018-07-26 08:59:51 +0545

    Added Pagination

--- src/component/products/ProductMain.jsx
+++ src/component/products/ProductMain.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './style/ProductMain.css';
 import ProductLists from "./ProductLists";
+import ProductMainPagination from "./ProductMainPagination";
 
 class ProductMain extends Component {
     render() {
         return (
             <div className={this.props.isOpen?"products-main-with-filter":"products-main"}>
                 <ProductLists isOpen={this.props.isOpen}/>
+                <ProductMainPagination/>
             </div>
         )
     }
---
+++ src/component/products/ProductMainPagination.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+
+class ProductMainPagination extends Component {
+    render() {
+        return (
+                <Pagination className="products-main-pagination" aria-label="Page navigation example">
+                    <PaginationItem disabled>
+                        <PaginationLink previous href="#" />
+                    </PaginationItem>
+                    <PaginationItem active>
+                        <PaginationLink href="#">
+                            1
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">
+                            2
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">
+                            3
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">
+                            4
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">
+                            5
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink next href="#" />
+                    </PaginationItem>
+                </Pagination>
+        )
+    }
+}
+
+export default ProductMainPagination;
